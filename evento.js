5d77a2af4614c681dadd892e4912832629220456
javier orayen <jorayen91@gmail.com>
2023-01-30 23:09:03 -0300

    segundo commit

---
+++ evento.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    alert("Bienvenido a mi Curriculum");
+  }; 
+
+  document.getElementById("miBoton").addEventListener("click", function() {
+    alert("Hola Mundo!");
+  });
